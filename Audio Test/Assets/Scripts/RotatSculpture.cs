dff72f0b949d56ba8226404d821d088c18cd66c1
KimoSlice <karhegazy@gmail.com>
2020-05-11 18:13:05 +0200

    First Commit

---
+++ Audio Test/Assets/Scripts/RotatSculpture.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatSculpture : MonoBehaviour
+{
+    public float rotationX = 100f;
+    public float rotationY = 100f;
+    public float rotationZ = 100f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotationX, rotationY, rotationZ) *Time.deltaTime);
+        
+    }
+}
